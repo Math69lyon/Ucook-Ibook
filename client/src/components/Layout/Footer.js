2e6d2abfdbdb9584c45682da2952daae75954355
gautier <gautier>
2020-06-17 16:02:15 +0200

    css

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core/styles'
+import { blue } from '@material-ui/core/colors'
+
+
+
+
+
+
 
 const styles = {
     root: {
         textAlign: 'center',
         margin: 20,
-        backgroundColor: '#607d8b'  
+        backgroundColor: '#2D4FAE' 
     }
 }
 
 const Footer = ({ classes }) => (
     <div className={classes.root}>
         <a href="https://facebook.com" title="Consultez nous sur facebook !" target="_blank"><img src="/Logos/facebook.jpg" alt="facebook"/></a>
-        <a href="https://instagram.com" title="Suivez nous sur facebook !" target="_blank"><img src="/Logos/instagram.jpeg" alt="instagram"/></a>
+        <a href="https://instagram.com" title="Suivez nous sur instagram !" target="_blank"><img src="/Logos/instagram.jpeg" alt="instagram"/></a>
     </div>
 )
 
